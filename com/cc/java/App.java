1dc223c7b56dec36c9cf4d7c1a768dafebe63682
Daniela <Daniela-Haeussler@gmx.de>
2022-11-29 10:37:36 +0100

    Output and new Methods

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,19 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+     Worker worker= new Worker();
+     Queen queen= new Queen();
+     Drone drone= new Drone();
+
+     output(worker.doYourJob());
+     output(queen.doYourJob());
+     output(drone.doYourJob());
+
+    output("---------");
+    output(worker.fly());
+    output(queen.fly());
+    output(drone.fly());
+
     }
 
     public static void output(String outputStr) {
